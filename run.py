b90145aba1d8b0f29974bd902be8c257f2a11089
psp <jiazaiwanbi@gmail.com>
2026-03-01 19:52:19 +0800

    change basedir

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ package_root = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 # 切换工作目录
 os.chdir(package_root)
 
-# 调用 python -m embykeeper
-subprocess.run([sys.executable, "-m", "embykeeper","-i"])
+# 调用 python -m embykeeper，并传递收到的参数
+subprocess.run([sys.executable,  "-m", "embykeeper", "-i", "-B", "./data"])
